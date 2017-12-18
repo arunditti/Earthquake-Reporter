03c1ea3da679a9d3ec878cfd6d8cca25421d1c6e
arunditti <arunditti@gmail.com>
2017-12-18 10:37:40 -0500

    Created Settings Activity

--- app/src/main/java/com/arunditti/android/earthquakereporter/MainActivity.java
+++ app/src/main/java/com/arunditti/android/earthquakereporter/MainActivity.java
@@ -12,6 +12,8 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -39,6 +41,22 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
     //TextView that is displayed when the list is empty
     private TextView mEmptyStateTextView;
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @ Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/arunditti/android/earthquakereporter/SettingsActivity.java
@@ -0,0 +1,21 @@
+package com.arunditti.android.earthquakereporter;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by arunditti on 12/18/17.
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+
+    }
+}
